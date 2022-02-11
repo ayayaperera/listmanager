c2ea7701f6ffc8f87af97e24faa3cc23c6b5cd6f
Luccas <monkeydluccas@gmail.com>
2022-02-11 08:21:38 +0000

    aaaaaaaa

--- src/Controller/ListasController.php
+++ src/Controller/ListasController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Checkbox;
 use App\Entity\Lista;
+use App\Form\CheckboxFormType;
 use App\Repository\CheckboxRepository;
 use App\Repository\ListaRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -13,10 +17,12 @@ use Twig\Environment;
 class ListasController extends AbstractController
 {
     private $twig;
+    private $entityManager;
 
-    public function __construct(Environment $twig)
+    public function __construct(Environment $twig, EntityManagerInterface $entityManager)
     {
         $this->twig = $twig;
+        $this->entityManager = $entityManager;
     }
 
     #[Route('/', name: 'homepage')]
@@ -28,12 +34,25 @@ class ListasController extends AbstractController
     }
 
     #[Route('/lista/{id}', name: 'lista')]
-    public function show(Lista $lista, ListaRepository $listaRepository, CheckboxRepository $checkboxRepository): Response
+    public function show(Request $request, Lista $lista, ListaRepository $listaRepository, CheckboxRepository $checkboxRepository): Response
     {
+        $checkbox = new Checkbox();
+        $form = $this->createForm(CheckboxFormType::class, $checkbox);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $checkbox->setLista($lista);
+
+            $this->entityManager->persist($checkbox);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('lista', ['id' => $lista->getId()]);
+        }
+
         return new Response($this->twig->render('listas/show.html.twig', [
             'listas' => $listaRepository->findAll(),
             'lista' => $lista,
             'checkboxes' => $checkboxRepository->findBy(['lista' => $lista]),
+            'checkbox_form' => $form->createView(),
         ]));
     }
 }
--- src/Entity/Checkbox.php
+++ src/Entity/Checkbox.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\CheckboxRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CheckboxRepository::class)]
 class Checkbox
@@ -14,6 +15,7 @@ class Checkbox
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank]
     private $nombre;
 
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
